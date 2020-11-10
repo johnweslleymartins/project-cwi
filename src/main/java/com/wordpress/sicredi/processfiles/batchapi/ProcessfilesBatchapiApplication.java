c9c5176346e009269f7cc3f3bae58a2a3da83f05
John Weslley <johnweslley010@gmail.com>
2020-11-10 10:44:31 -0300

    commit test

--- src/main/java/com/wordpress/sicredi/processfiles/batchapi/ProcessfilesBatchapiApplication.java
+++ src/main/java/com/wordpress/sicredi/processfiles/batchapi/ProcessfilesBatchapiApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ProcessfilesBatchapiApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ProcessfilesBatchapiApplication.class, args);
 	}
 }
